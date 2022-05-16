e57863a12a81d16f96d33654ee2a7d79ae3471bc
Musabii Mambetov <81331273+musabiii@users.noreply.github.com>
2022-05-16 11:40:20 +0300

    emit from front is work

--- src-front/src/index.js
+++ src-front/src/index.js
@@ -4,3 +4,13 @@ import {invoke} from "@tauri-apps/api/tauri";
 listen('tauri://resize',(e)=>{
     console.log('resize')
 })
+
+listen("somaemit",(e)=>{
+    console.log('its some emit')
+})
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click',(e)=>{
+    console.log('btn start');
+    invoke('my_custom_command');
+})
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,25 @@
-#![cfg_attr(
+use std::process::Command;
+
+// #![cfg_attr(
+#[cfg_attr(
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
 
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![my_custom_command])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
+
+#[tauri::command]
+fn my_custom_command() {
+  println!("I was invoked from JS!");
+
+  Command::new("cmd")
+  .args(["/C", "start calc"])
+  .output()
+  .expect("failed to execute process");
+}
+
